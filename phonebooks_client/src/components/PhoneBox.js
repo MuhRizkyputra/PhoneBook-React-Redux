cd53fb2b27cde9a340eedcb6ec16c9ced8704c7b
Muhammad <muhammadrizkyputraaa@gmail.com>
2024-03-04 21:44:16 +0700

    Create Components Phone Box

--- phonebooks_client/src/components/PhoneBox.js
+++ phonebooks_client/src/components/PhoneBox.js
@@ -1,10 +1,24 @@
-import PhoneList from "./PhoneList";
+import SearchBar from "./SearchBar";
+import Phonelist from "./PhoneList";
+import { useState } from "react";
+
 
 
 export default function PhoneBox() {
+
+    const [keyword, setKeyword] = useState('');
+    const [sort, setSort] = useState('asc');
+
+
     return (
-        <>
-            <PhoneList />
-        </>
+        <div className="container">
+            <div className="header">
+                <SearchBar keyword={keyword} setKeyword={setKeyword} sort={sort} setSort={setSort}/>
+            </div>
+            <div className="body">
+                <Phonelist keyword={keyword} sort={sort}/>
+            </div>
+
+        </div>
     )
 }
