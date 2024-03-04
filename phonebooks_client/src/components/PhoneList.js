aa30e183e5a2fe3823201db9620e17c8d845ba90
Muhammad <muhammadrizkyputraaa@gmail.com>
2024-03-04 21:45:22 +0700

    Create Components Phone List

--- phonebooks_client/src/components/PhoneList.js
+++ phonebooks_client/src/components/PhoneList.js
@@ -3,26 +3,20 @@ import { selectPhonebooks } from "../reducers/phonebook"
 import PhoneItem from "./PhoneItem";
 import { useEffect } from "react";
 import { loadPhonebooks } from "../reducers/API";
+import { faSort } from "@fortawesome/free-solid-svg-icons";
 
-export default function PhoneList() {
+export default function PhoneList({ keyword, sort }) {
     const dispatch = useDispatch()
     const { phonebook } = useSelector(selectPhonebooks)
 
 
     useEffect(() => {
-        const readData = async () => {
-            try {
-                dispatch(loadPhonebooks())
-            } catch (error) {
-                console.log(error)
-            }
-        }
-        readData()
-    }, [dispatch])
+        dispatch(loadPhonebooks({ keyword, sort }))
+    }, [dispatch, keyword, sort])
 
     return (
-        <div className="mainList" id="main-data">
-            {!!phonebook && phonebook.length > 0 && phonebook.map((user) => (
+        <div className="phonelist" id="main-data">
+            {phonebook.map((user) => (
                 <PhoneItem key={user.id} user={user} />
             ))}
         </div>
