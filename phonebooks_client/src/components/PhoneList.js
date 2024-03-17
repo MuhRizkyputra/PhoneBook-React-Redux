d6649d109a9538145e69a7128dbd8573add862e6
Muhammad <muhammadrizkyputraaa@gmail.com>
2024-03-17 11:29:43 +0700

    Create Component Phone List

--- phonebooks_client/src/components/PhoneList.js
+++ phonebooks_client/src/components/PhoneList.js
@@ -1,14 +1,39 @@
 import { useDispatch, useSelector } from "react-redux";
 import { selectPhonebooks } from "../reducers/phonebook"
 import PhoneItem from "./PhoneItem";
-import { useEffect } from "react";
-import { loadPhonebooks } from "../reducers/API";
-import { faSort } from "@fortawesome/free-solid-svg-icons";
+import { useEffect, useState } from "react";
+import { loadPage, loadPhonebooks } from "../reducers/API";
 
 export default function PhoneList({ keyword, sort }) {
     const dispatch = useDispatch()
-    const { phonebook } = useSelector(selectPhonebooks)
+    const { phonebook, page, pages } = useSelector(selectPhonebooks)
+    const [isLoading, setIsLoading] = useState(false)
 
+    const handelScroll = async () => {
+        if (window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight && !isLoading) {
+            try {
+                if (page < pages) {
+                    setIsLoading(true)
+                    const newPage = page + 1
+                    dispatch(loadPage({ page: newPage, keyword, sort }))
+                }
+                else {
+                    setIsLoading(false)
+                }
+            } catch (error) {
+                console.log(error)
+            } finally {
+                setIsLoading(false)
+            }
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener('scroll', handelScroll)
+        return () => {
+            window.removeEventListener('scroll', handelScroll)
+        }
+    }, [dispatch, pages, page, keyword, sort])
 
     useEffect(() => {
         dispatch(loadPhonebooks({ keyword, sort }))
